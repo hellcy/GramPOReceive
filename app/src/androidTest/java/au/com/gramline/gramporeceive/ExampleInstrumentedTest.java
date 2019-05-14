45705ed39dcbd572626769ca7174dadc4295db6f
yuan cheng <chengyuan82281681@hotmail.com>
2019-05-14 15:27:46 +1000

    add automate tests

--- app/src/androidTest/java/au/com/gramline/gramporeceive/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/au/com/gramline/gramporeceive/ExampleInstrumentedTest.java
@@ -1,13 +1,24 @@
 package au.com.gramline.gramporeceive;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+import android.app.Activity;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,11 +27,40 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("au.com.gramline.gramporeceive", appContext.getPackageName());
+    public static final String Username = "Charles Cheng";
+    public static final String Password = "12345678";
+
+    @Rule public ActivityScenarioRule<MainActivity> activityScenarioRule
+            = new ActivityScenarioRule<>(MainActivity.class);
+
+//    @Test
+//    public void changeText_sameActivity() {
+//        // Type text and then press the button.
+//        onView(withId(R.id.userName))
+//                .perform(typeText(Username), closeSoftKeyboard());
+//        onView(withId(R.id.password))
+//                .perform(typeText(Password), closeSoftKeyboard());
+//        onView(withId(R.id.loginBtn)).perform(click());
+//
+//        // Check that the text was changed.
+//        //onView(withId(R.id.textToBeChanged)).check(matches(withText(STRING_TO_BE_TYPED)));
+//    }
+
+    @Test
+    public void changeText_newActivity() {
+        // Type text and then press the button.
+        onView(withId(R.id.userName))
+                .perform(typeText(Username), closeSoftKeyboard());
+        onView(withId(R.id.password))
+                .perform(typeText(Password), closeSoftKeyboard());
+        onView(withId(R.id.loginBtn)).perform(click());
+
+        onView(withId(R.id.enterOrderNumberText)).perform(typeText("11986"),
+                closeSoftKeyboard());
+        onView(withId(R.id.getOrdersButton)).perform(click());
+
+        // This view is in a different Activity, no need to tell Espresso.
+        //onView(withId(R.id.show_text_view)).check(matches(withText(STRING_TO_BE_TYPED)));
     }
 }
---
+++ app/src/androidTest/java/au/com/gramline/gramporeceive/MainActivityTest.java
@@ -0,0 +1,5 @@
+package au.com.gramline.gramporeceive;
+
+public class MainActivityTest {
+
+}
